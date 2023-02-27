a1dbd22fe329cdbca842d80f93439ed8bcf7361c
kyriakouk <121340556+kyriakouk@users.noreply.github.com>
2023-02-27 19:51:31 +0200

    Update BookApi.java

--- src/main/java/services/BookApi.java
+++ src/main/java/services/BookApi.java
@@ -2,7 +2,7 @@ package services;
 
 public class BookApi {
 
-	public static final String API_KEY="AIzaSyBj0UjiEoNRsJHJPpbY6kt0Fa8CGiQR70o";
+	public static final String API_KEY="your api key";
 	public static final String API_URL="www.googleapis.com";
 }
 
